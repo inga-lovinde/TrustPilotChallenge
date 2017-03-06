4d02d3e7764ea02a5d9ddc123104fa3cb3fd34b2
inga-lovinde <52715130+inga-lovinde@users.noreply.github.com>
2017-03-06 10:28:03 +0300

    Initial project structure

---
+++ WhiteRabbit/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace WhiteRabbit
+{
+    /// <summary>
+    /// Main class
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Main entry point
+        /// </summary>
+        public static void Main()
+        {
+        }
+    }
+}
