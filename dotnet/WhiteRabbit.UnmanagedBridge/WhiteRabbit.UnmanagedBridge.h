0090bce4438c28bb520d32fcdb5e07985995da8c
inga-lovinde <52715130+inga-lovinde@users.noreply.github.com>
2017-04-26 14:48:17 +0300

    NumberOfPhrases moved out to UnmanagedBridge

--- dotnet/WhiteRabbit.UnmanagedBridge/WhiteRabbit.UnmanagedBridge.h
+++ dotnet/WhiteRabbit.UnmanagedBridge/WhiteRabbit.UnmanagedBridge.h
@@ -9,6 +9,7 @@ namespace WhiteRabbitUnmanagedBridge {
 	public ref class MD5Unmanaged
 	{
         public:
+            literal int PhrasesPerSet = 16;
             static void ComputeMD5(unsigned int* input, unsigned int* output);
 	};
 }
--- dotnet/WhiteRabbit/Constants.cs
+++ dotnet/WhiteRabbit/Constants.cs
@@ -2,6 +2,6 @@
 {
     internal class Constants
     {
-        public const int PhrasesPerSet = 16;
+        public const int PhrasesPerSet = WhiteRabbitUnmanagedBridge.MD5Unmanaged.PhrasesPerSet;
     }
 }
